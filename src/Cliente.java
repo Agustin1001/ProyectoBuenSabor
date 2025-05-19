9efe6f4ec66d92dc97a08a0b8b4cd83141f6db83
adriezeh <a.ezequias.herrera@gmail.com>
2025-05-19 14:30:28 -0300

    Usuario Cliente y Rol Agregados

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,11 @@
 public class Cliente {
-}
+        String nombre;
+        String apellido;
+        String telefono;
+        String email;
+        LocalDate fechaNacimiento;
+
+        Usuario usuario;   // N:1
+        Imagen imagen;     // 1:1
+    }
+
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,8 @@
 public class Usuario {
-}
+        String auth0Id;
+        String username;
+
+        // Opcional si se implementa la relación inversa (no obligatoria según el modelo)
+        Set<Cliente> clientes;
+    }
+
