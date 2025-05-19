d78dbe72db2bd2b423a9207a57aacb804e3f8a26
adriezeh <a.ezequias.herrera@gmail.com>
2025-05-19 14:33:20 -0300

    Update Usuario.java

--- src/Usuario.java
+++ src/Usuario.java
@@ -2,7 +2,6 @@ public class Usuario {
         String auth0Id;
         String username;
 
-        // Opcional si se implementa la relación inversa (no obligatoria según el modelo)
         Set<Cliente> clientes;
     }
 
